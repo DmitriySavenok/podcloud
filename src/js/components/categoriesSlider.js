fc63f61db24cf42ebb89ef0d432596c9aca271c5
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2024-02-21 01:31:55 +0300

    categories list animation

--- src/js/components/categoriesSlider.js
+++ src/js/components/categoriesSlider.js
@@ -1,13 +1,23 @@
 import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
 
-function categoriseSlider() {
-	const swiper = new Swiper('.swiper', {
-		direction: 'horizontal',
-		loop: true
-	});
+function categoriesSlider() {
+	// const swiper = new Swiper('.swiper', {
+	// 	modules: [Autoplay],
+	// 	slidesPerView: 4.5,
+	// 	freeMode: true,
+	// 	loop: true,
+	// 	autoplay: {
+	// 		delay: 0,
+	// 		pauseOnMouseEnter: true
+	// 	},
+	// 	grabCursor: true,
+	// 	speed: 7000,
+	// 	spaceBetween: 120
+	// });
 	console.log('скрипт запустился')
 }
 
 export default {
-	categoriseSlider
+	categoriesSlider
 }
