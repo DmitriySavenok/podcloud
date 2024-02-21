e65180e4332644bfb60218db151bbe73897b6063
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2024-02-21 15:26:44 +0300

    categories slide fixed

--- src/js/components/categoriesSlider.js
+++ src/js/components/categoriesSlider.js
@@ -2,20 +2,19 @@ import Swiper from 'swiper';
 import { Autoplay } from 'swiper/modules';
 
 function categoriesSlider() {
-	// const swiper = new Swiper('.swiper', {
-	// 	modules: [Autoplay],
-	// 	slidesPerView: 4.5,
-	// 	freeMode: true,
-	// 	loop: true,
-	// 	autoplay: {
-	// 		delay: 0,
-	// 		pauseOnMouseEnter: true
-	// 	},
-	// 	grabCursor: true,
-	// 	speed: 7000,
-	// 	spaceBetween: 120
-	// });
-	console.log('скрипт запустился')
+	const swiper = new Swiper('.categories-slide', {
+		modules: [Autoplay],
+		slidesPerView: 5,
+		freeMode: true,
+		loop: true,
+		autoplay: {
+			delay: 1,
+			pauseOnMouseEnter: true
+		},
+		grabCursor: true,
+		speed: 5000,
+		spaceBetween: 120
+	});
 }
 
 export default {
